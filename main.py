4ef58930ff65f994b6db97123d0e96f386aeede4
Everett Butler <64377136+everettbu@users.noreply.github.com>
2023-06-07 11:21:46 -0700

    favorite and skip button implemented

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import kivy
 from kivy.app import App
-from kivy.uix.stacklayout import StackLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.image import AsyncImage
+from kivy.uix.button import Button
 from fetch import fetch_restaurants
 
 # Bypasses SSL verification (needs to be fixed)
@@ -14,24 +15,62 @@ kivy.require('2.0.0')
 
 class Tender(App):
     def build(self):
-        layout = StackLayout()
+        layout = BoxLayout(orientation='vertical')
+        self.restaurants = fetch_restaurants()
+        self.current_index = 0
 
-        for restaurant in fetch_restaurants():
-            label = Label(text=restaurant['name'])
-            image = AsyncImage(source=restaurant['image_url'])
+        self.label = Label(text=self.restaurants[self.current_index]['name'])
+        self.image = AsyncImage(source=self.restaurants[self.current_index]['image_url'])
+        self.restaurant_widget = BoxLayout(orientation='vertical')
+        self.restaurant_widget.add_widget(self.image)
+        self.restaurant_widget.add_widget(self.label)
 
-            restaurant_widget = StackLayout()
-            restaurant_widget.add_widget(image)
-            restaurant_widget.add_widget(label)
+        layout.add_widget(self.restaurant_widget)
 
-            layout.add_widget(restaurant_widget)
+        # Button layout
+        button_layout = BoxLayout(orientation='horizontal', size_hint=(1, 0.1))
+
+        # Favorite Button
+        favorite_button = Button(text="Favorite", size_hint=(0.5, None), height=50)
+        favorite_button.bind(on_press=self.favorite_restaurant)
+        button_layout.add_widget(favorite_button)
+
+        # Skip Button
+        skip_button = Button(text="Skip", size_hint=(0.5, None), height=50)
+        skip_button.bind(on_press=self.skip_restaurant)
+        button_layout.add_widget(skip_button)
+
+        layout.add_widget(button_layout)
 
         return layout
 
-    def favorite_restaurant(self, restaurant):
-        # TODO: Add restaurant to user's favorites list.
-        pass
+    def favorite_restaurant(self, instance):
+        # Add restaurant to user's favorites list
+        restaurant = self.restaurants[self.current_index]
+        # Implement your logic to add the restaurant to favorites
+        print(f"Restaurant '{restaurant['name']}' added to favorites.")
 
+        # Move to the next restaurant
+        self.current_index += 1
+
+        if self.current_index < len(self.restaurants):
+            self.label.text = self.restaurants[self.current_index]['name']
+            self.image.source = self.restaurants[self.current_index]['image_url']
+        else:
+            self.label.text = "No more restaurants"
+            self.image.source = ""
+
+    def skip_restaurant(self, instance):
+        # Move to the next restaurant
+        self.current_index += 1
+
+        if self.current_index < len(self.restaurants):
+            self.label.text = self.restaurants[self.current_index]['name']
+            self.image.source = self.restaurants[self.current_index]['image_url']
+        else:
+            self.label.text = "No more restaurants"
+            self.image.source = ""
 
 if __name__ == '__main__':
     Tender().run()
+    
